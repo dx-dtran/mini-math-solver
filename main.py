6dd39ad46a252d32e7e65c767730f2ef1cbea688
daniel <dxdtran@outlook.com>
2023-09-24 18:22:58 -0700

    update learning rate printing

--- main.py
+++ main.py
@@ -103,6 +103,7 @@ start = time.time()
 for epoch in range(num_epochs):
     student_model.train()
     running_loss = 0.0
+    current_lr = 0.0
     for i, batch in enumerate(train_loader):
         optimizer.zero_grad()
 
@@ -134,8 +135,8 @@ for epoch in range(num_epochs):
             scheduler.step()
 
             current_lr = optimizer.param_groups[0]['lr']
-            print(f'Current learning rate: {current_lr}')
 
+    print(f'Current learning rate: {current_lr}')
     print(f'Training loss epoch {epoch + 1}: {running_loss / len(train_loader)}')
     print('time: {:0.2f} seconds'.format(time.time() - start))
 
@@ -168,7 +169,7 @@ for epoch in range(num_epochs):
     val_acc = compute_equation_acc(val_preds, val_labels)
 
     for i, j, k in zip(inputs_list[:5], expl_list[:5], val_preds[:5]):
-        print('prompt: {}:\nexplain: {}\nequation: {}\n\n'.format(i, j, k))
+        print('prompt: {}:\nexplain: {}\nequation: {}\n\n'.format(i.split('predict: ')[0], j, k))
 
     print(f'Validation Accuracy epoch {epoch + 1}, Accuracy: {val_acc}')
     print('time: {:0.2f} seconds'.format(time.time() - start))
