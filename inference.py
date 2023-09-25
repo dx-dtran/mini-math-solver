4b749138d460e91f0f5a5465b7a8a14f9bd66574
daniel <dxdtran@outlook.com>
2023-09-24 20:44:03 -0700

    add explanations in inference

--- inference.py
+++ inference.py
@@ -4,15 +4,21 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 tokenizer = AutoTokenizer.from_pretrained("checkpoints")
 model = AutoModelForSeq2SeqLM.from_pretrained("checkpoints")
 
-# Replace 'your_input_text' with the actual text you want to use
-input_text = "predict: I have 30 apples. My friend has 20 apples. How many apples do we have total?"
-inputs = tokenizer(input_text, return_tensors="pt", truncation=True, padding=True)
+for _ in range(10):
+    prompt = input("enter a prompt: ")
+    pred_text = "predict: {}".format(prompt)
+    expl_text = "explain: {}".format(prompt)
+    inputs = tokenizer(pred_text, return_tensors="pt", truncation=True, padding=True)
+    expls = tokenizer(expl_text, return_tensors="pt", truncation=True, padding=True)
 
-# Generate a prediction
-outputs = model.generate(**inputs)
+    # Generate a prediction
+    outputs = model.generate(**inputs, max_new_tokens=32)
+    expl_outputs = model.generate(**expls, max_new_tokens=32)
 
-# Convert the output to text
-predicted_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    # Convert the output to text
+    predicted_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    expl_out_text = tokenizer.decode(expl_outputs[0], skip_special_tokens=True)
 
-# For example, print the predicted text
-print(predicted_text)
+    # For example, print the predicted text
+    print(expl_out_text)
+    print("the answer is: ", predicted_text)
