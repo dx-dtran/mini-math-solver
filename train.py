29e9e00c891de5b7b3faa652d4b7fbb6578f382b
daniel <dxdtran@outlook.com>
2023-10-01 20:15:19 -0700

    simplify training loop

--- train.py
+++ train.py
@@ -6,72 +6,76 @@ from transformers import AutoTokenizer, T5ForConditionalGeneration
 from torch.utils.data import DataLoader
 
 
-def eval_equation(equation):
-    try:
-        answer = eval(equation)
-    except:
-        answer = np.nan
-    return answer
-
-
-def compute_equation_acc(preds, labels):
-    preds = [eval_equation(pred) for pred in preds]
-    labels = [eval_equation(label) for label in labels]
-    return np.mean(np.array(preds) == np.array(labels))
-
-
 def tokenize_function(examples):
-    # Assuming that your data has 'pred', 'expl', 'label', and 'rationale' as the keys
-    pred_encodings = tokenizer(
-        examples['pred'], truncation=True, padding='max_length', max_length=128, return_tensors='pt'
+    # Assuming that your data has 'problem', 'explain', 'formula', and 'reason' as the keys
+    problem_encodings = tokenizer(
+        "predict: " + examples['problem'], truncation=True, padding='max_length', max_length=128, return_tensors='pt'
     )
     expl_encodings = tokenizer(
-        examples['expl'], truncation=True, padding='max_length', max_length=128, return_tensors='pt'
+        "explain: " + examples['problem'], truncation=True, padding='max_length', max_length=128, return_tensors='pt'
     )
-    label_encodings = tokenizer(
-        examples['label'], truncation=True, padding='max_length', max_length=128, return_tensors='pt'
+    formula_encodings = tokenizer(
+        examples['formula'], truncation=True, padding='max_length', max_length=128, return_tensors='pt'
     )
-    rationale_encodings = tokenizer(
-        examples['rationale'], truncation=True, padding='max_length', max_length=128, return_tensors='pt'
+    reasoning_encodings = tokenizer(
+        examples['reason'], truncation=True, padding='max_length', max_length=128, return_tensors='pt'
     )
 
     return {
-        'pred': pred_encodings,
-        'expl': expl_encodings,
-        'label': label_encodings,
-        'rationale': rationale_encodings
+        'problem': problem_encodings,
+        'explain': expl_encodings,
+        'formula': formula_encodings,
+        'reason': reasoning_encodings
     }
 
 
-def collate_fn(batch):
+def collate_function(batch):
     def stack_encoding(key):
         return {
             'input_ids': torch.stack([torch.tensor(item[key]['input_ids']).squeeze() for item in batch]),
             'attention_mask': torch.stack([torch.tensor(item[key]['attention_mask']).squeeze() for item in batch]),
         }
 
-    pred = stack_encoding('pred')
-    expl = stack_encoding('expl')
-    label_input_ids = torch.stack([torch.tensor(item['label']['input_ids']).squeeze() for item in batch])
-    rationale_input_ids = torch.stack([torch.tensor(item['rationale']['input_ids']).squeeze() for item in batch])
+    def stack_tensors(key):
+        return torch.stack([torch.tensor(item[key]['input_ids']).squeeze() for item in batch])
 
     return {
-        'pred': pred,
-        'expl': expl,
-        'label': {'input_ids': label_input_ids},
-        'rationale': {'input_ids': rationale_input_ids},
+        'problem': stack_encoding('problem'),
+        'explain': stack_encoding('explain'),
+        'formula': {'input_ids': stack_tensors('formula')},
+        'reason': {'input_ids': stack_tensors('reason')},
     }
+
+
+def get_training_example(batch, key, device):
+    inputs = batch[key]['input_ids'].to(device)
+    attn_mask = batch[key]['attention_mask'].to(device)
+    return inputs, attn_mask
+
+
+def eval_formula(formula):
+    try:
+        answer = eval(formula)
+    except:
+        answer = np.nan
+    return answer
+
+
+def compute_formula_acc(problems, formulas):
+    problems = [eval_formula(problem) for problem in problems]
+    formulas = [eval_formula(formula) for formula in formulas]
+    return np.mean(np.array(problems) == np.array(formulas))
 
 
 # Checking for GPU availability
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f'Using {device} device')
 
-tokenizer = AutoTokenizer.from_pretrained('google/flan-t5-base')
-student_model = T5ForConditionalGeneration.from_pretrained('google/flan-t5-base')
+tokenizer = AutoTokenizer.from_pretrained('google/t5-v1_1-base')
+student_model = T5ForConditionalGeneration.from_pretrained('google/t5-v1_1-base')
 student_model = student_model.to(device)
 
-dataset = load_dataset('json', data_files='svamp_train.json', split='train')
+dataset = load_dataset('json', data_files='data_train.json', split='train')
 
 # Split the dataset
 train_val_dataset = dataset.train_test_split(test_size=0.1)
@@ -83,8 +87,8 @@ tokenized_val_dataset = train_val_dataset["test"].map(tokenize_function)
 BATCH_SIZE = 8
 
 # Create DataLoaders to handle batching
-train_loader = DataLoader(tokenized_train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
-val_loader = DataLoader(tokenized_val_dataset, batch_size=BATCH_SIZE, shuffle=False, collate_fn=collate_fn)
+train_loader = DataLoader(tokenized_train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_function)
+val_loader = DataLoader(tokenized_val_dataset, batch_size=BATCH_SIZE, shuffle=False, collate_fn=collate_function)
 
 num_epochs = 200
 accumulation_steps = 8
@@ -109,19 +113,16 @@ for epoch in range(num_epochs):
     for i, batch in enumerate(train_loader):
         optimizer.zero_grad()
 
-        # Move batch data to the same device as the model (i.e., GPU if available)
-        pred_inputs = batch['pred']['input_ids'].to(device)
-        pred_attention_mask = batch['pred']['attention_mask'].to(device)
-        pred_labels = batch['label']['input_ids'].to(device)
+        pred_inputs, pred_attention_mask = get_training_example(batch, 'problem', device)
+        pred_labels, _ = get_training_example(batch, 'formula', device)
 
-        expl_inputs = batch['expl']['input_ids'].to(device)
-        expl_attention_mask = batch['expl']['attention_mask'].to(device)
-        expl_labels = batch['rationale']['input_ids'].to(device)
+        expl_inputs, expl_attention_mask = get_training_example(batch, 'explain', device)
+        expl_labels, _ = get_training_example(batch, 'reason', device)
 
-        # Forward pass for prediction
+        # Forward pass for predicting the result as a formula
         pred_outputs = student_model(input_ids=pred_inputs, attention_mask=pred_attention_mask, labels=pred_labels)
 
-        # Forward pass for explanation
+        # Forward pass for explaining the result
         expl_outputs = student_model(input_ids=expl_inputs, attention_mask=expl_attention_mask, labels=expl_labels)
 
         # Combined loss
@@ -150,16 +151,16 @@ for epoch in range(num_epochs):
         inputs_list, expl_list = [], []
         with torch.no_grad():
             for i, batch in enumerate(val_loader):
-                pred_inputs = batch['pred']['input_ids'].to(device)
-                pred_attention_mask = batch['pred']['attention_mask'].to(device)
+                pred_inputs, pred_attention_mask = get_training_example(batch, 'problem', device)
+                pred_labels, _ = get_training_example(batch, 'formula', device)
 
-                pred_labels = batch['label']['input_ids'].to(device)
+                expl_inputs, expl_attention_mask = get_training_example(batch, 'explain', device)
+                expl_labels, _ = get_training_example(batch, 'reason', device)
+
                 pred_outputs = student_model.generate(
                     input_ids=pred_inputs, attention_mask=pred_attention_mask, max_new_tokens=32
                 )
 
-                expl_inputs = batch['expl']['input_ids'].to(device)
-                expl_attention_mask = batch['expl']['attention_mask'].to(device)
                 expl_outputs = student_model.generate(
                     input_ids=expl_inputs, attention_mask=expl_attention_mask, max_new_tokens=32
                 )
@@ -170,9 +171,9 @@ for epoch in range(num_epochs):
                 inputs_list.extend(tokenizer.batch_decode(pred_inputs, skip_special_tokens=True))
                 expl_list.extend(tokenizer.batch_decode(expl_outputs, skip_special_tokens=True))
 
-        val_acc = compute_equation_acc(val_preds, val_labels)
+        val_acc = compute_formula_acc(val_preds, val_labels)
 
-        for i, j, k in zip(inputs_list[:5], expl_list[:5], val_preds[:5]):
+        for i, j, k in zip(inputs_list[:8], expl_list[:8], val_preds[:8]):
             print('prompt: {}\nthought: {}\nresult: {}\n\n'.format(i.split('predict: ')[1], j, k))
 
         print(f'Validation Accuracy epoch {epoch + 1}, Accuracy: {val_acc}')
